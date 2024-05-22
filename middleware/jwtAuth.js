d9af94313718eb7f60d6f1c4b42265ebd8cd7aa3
Arpan Tomar <arpantomar2018@gmail.com>
2024-05-22 17:47:57 +0530

    implemented authentication for login and register

---
+++ middleware/jwtAuth.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const jwtAuth = (req, res, next) => {
+  const token = req.header("x-auth-token");
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied" });
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
+
+export default jwtAuth;
